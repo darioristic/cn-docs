0c93be85aea28c3effe7d3fb36f230d816e573ea
darioristic <darioristic@gmail.com>
2025-08-29 15:33:40 +0200

    Fix theme configuration and resolve missing components error

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,30 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Cloud Native Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/darioristic/cn-docs',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/darioristic/cn-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Cloud Native Internal Documentation',
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Cloud Native Docs'
+    }
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Cloud Native Internal Documentation" />
+    </>
+  ),
+  primaryHue: 210,
+  primarySaturation: 100,
 }
 
 export default config
